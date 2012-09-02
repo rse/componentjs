75f0428b35fc4e3cfd1399845e4c289fc3477c6e
Ralf S. Engelschall <rse@engelschall.com>
2012-09-02 18:02:18 +0000

    improve socket functionality by making it more orthogonal and remembering plug information

--- component-3-patt-C-socket.js
+++ component-3-patt-C-socket.js
@@ -13,12 +13,16 @@ $cs.pattern.socket = $cs.trait({
         $cs.pattern.tree,
         $cs.pattern.property
     ],
+    dynamics: {
+        __plugs: {},
+        __plugs_id: 0
+    },
     protos: {
         /*  define a socket  */
         socket: function () {
             /*  determine parameters  */
             var params = $cs.params("socket", arguments, {
-                name:   {         def: null            },
+                name:   {         def: "default"       },
                 scope:  {         def: null            },
                 ctx:    { pos: 0, def: null, req: true },
                 plug:   { pos: 1, def: null, req: true },
@@ -40,9 +44,7 @@ $cs.pattern.socket = $cs.trait({
                 throw _cs.exception("socket", "unplug operation neither method name nor function");
 
             /*  remember parameters as (optionally scoped) component property  */
-            var name = "socket";
-            if (params.name !== null)
-                name += ":" + params.name;
+            var name = "ComponentJS:socket:" + params.name;
             if (params.scope !== null)
                 name += "@" + params.scope;
             $cs(this).property(name, params);
@@ -50,31 +52,54 @@ $cs.pattern.socket = $cs.trait({
 
         /*  plug into a defined socket  */
         plug: function () {
-            return _cs.plugger("plug", this, false, arguments);
-        },
-        plugInto: function () {
-            return _cs.plugger("plug", this, true, arguments);
+            /*  determine parameters  */
+            var params = $cs.params("plug", arguments, {
+                name:     {         def: "default"           },
+                object:   { pos: 0,                req: true },
+                remember: {         def: true                }
+            });
+
+            /*  optionally remember plug operation  */
+            if (params.remember)
+                this.__plugs[this.__plugs_id++] = { name: params.name, object: params.object };
+
+            /*  pass-though to common helper function  */
+            return _cs.plugger("plug", this, params.name, params.object);
         },
 
         /*  unplug from a defined socket  */
         unplug: function () {
-            return _cs.plugger("unplug", this, false, arguments);
-        },
-        unplugFrom: function () {
-            return _cs.plugger("unplug", this, true, arguments);
+            /*  determine parameters  */
+            var params = $cs.params("unplug", arguments, {
+                name:   {         def: "default"  },
+                object: { pos: 0, def: null       }
+            });
+
+            /*  optionally retrieve object from remembered plug operation  */
+            if (params.object === null) {
+                var id_remove = null;
+                for (var id in this.__plugs) {
+                    if (!_cs.isown(this.__plugs, id))
+                        continue;
+                    if (params.name === this.__plugs[id].name) {
+                        params.object = this.__plugs[id].object;
+                        id_remove = id;
+                        break;
+                    }
+                }
+                if (params.object === null)
+                    throw _cs.exception("unplug", "object to unplug neither given not previously remembered");
+                delete this.__plugs[id_remove];
+            }
+
+            /*  pass-though to common helper function  */
+            return _cs.plugger("unplug", this, params.name, params.object);
         }
     }
 });
 
 /*  internal "plug/unplug to socket" helper functionality  */
-_cs.plugger = function (op, origin, named, args) {
-    /*  determine socket property name  */
-    var property = "socket";
-    if (named) {
-        property += ":" + args[0];
-        args = _cs.filter(function (_, i) { return i >= 1; }, args);
-    }
-
+_cs.plugger = function (op, origin, name, object) {
     /*  resolve the socket property on the parents components
         NOTICE 1: we explicitly skip the origin component here as
                   resolving the socket property also on the origin
@@ -84,15 +109,16 @@ _cs.plugger = function (op, origin, named, args) {
         NOTICE 2: we intentionally skip the origin and do not directly
                   resolve on the parent component as we want to take
                   scoped sockets (on the parent component) into account!  */
+    var property = "ComponentJS:socket:" + name;
     var socket = origin.property({ name: property, skiporigin: true });
     if (!_cs.isdefined(socket))
         throw _cs.exception(op, "no socket found on parent component(s)");
 
     /*  perform plug/unplug operation  */
     if (_cs.istypeof(socket[op]) === "string")
-        return socket.ctx[socket[op]].apply(socket.ctx, args);
+        return socket.ctx[socket[op]].call(socket.ctx, object);
     else if (_cs.istypeof(socket[op]) === "function")
-        return socket[op].apply(socket.ctx, args);
+        return socket[op].call(socket.ctx, object);
     else
         throw _cs.exception(op, "unable to perform \"" + op + "\" operation");
 };
