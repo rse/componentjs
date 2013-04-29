8fcc8332a8b72928f92048610357e2025437ef6d
Ralf S. Engelschall <rse@engelschall.com>
2013-04-29 20:35:39 +0200

    add a convenience function for retriving keys (for use in plugins)

--- component-1-util-1-object.js
+++ component-1-util-1-object.js
@@ -81,6 +81,16 @@ _cs.istypeof = function (obj) {
     return type;
 };
 
+/*  utility function: retrieve keys of object  */
+_cs.keysof = function (obj) {
+    var keys = [];
+    for (var key in obj) {
+        if (obj.hasOwnProperty(key))
+            keys.push(key);
+    }
+    return keys;
+};
+
 /*  utility function: JSON encoding of object  */
 _cs.json = (function () {
     var escapable = /[\\\"\x00-\x1f\x7f-\x9f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g;
