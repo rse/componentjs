d3c16944620a2d7c58d04de00f3c536d461fab2d
Ralf S. Engelschall <rse@engelschall.com>
2013-12-31 23:23:06 +0100

    bump year in all copyright messages

--- src/core/component-1-util-4-validate.js
+++ src/core/component-1-util-4-validate.js
@@ -1,6 +1,6 @@
 /*
 **  ComponentJS -- Component System for JavaScript <http://componentjs.com>
-**  Copyright (c) 2009-2013 Ralf S. Engelschall <http://engelschall.com>
+**  Copyright (c) 2009-2014 Ralf S. Engelschall <http://engelschall.com>
 **
 **  This Source Code Form is subject to the terms of the Mozilla Public
 **  License, v. 2.0. If a copy of the MPL was not distributed with this
